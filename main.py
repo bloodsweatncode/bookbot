35111940eb9fb54ae2c714f7da70f10e2952fb0c
bloodsweatncode <christian.schmidt@bluedragon.de>
2025-03-06 17:41:28 +0100

    nearly finished

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-from stats import num_words
-
+from stats import get_num_words, get_chars_dict, chars_dict_to_sorted_list
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
+    
 
 def main():
-    print(f"{num_words(get_book_text('books/frankenstein.txt'))} words found in the document")
+    book_path = 'books/frankenstein.txt'
+    book = get_book_text(book_path)
+    word_count = get_num_words(book)
+    chars_dict = get_chars_dict(book)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, word_count, chars_sorted_list)
+    
+
+def print_report(book_path, word_count, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if item["character"].isalpha():
+            print(f"{item["character"]}: {item["count"]}")
+    print("============= END ===============")
+
 
 main()
-
-
--- stats.py
+++ stats.py
@@ -1,5 +1,37 @@
-def num_words(text):
+import string
+
+def get_num_words(text):
     counter = 0 
     for i in text.split():
         counter += 1
     return counter
+
+"""
+def analyze_characters(text):
+    character_set = {'$', 'q', 'z', 'f', 'o', '_', 'p', '9', '™', 'r', 'h', '1', '”', 'g', 'x', '[', 't', ':', 'ê', 'b', 'm', ')', 'l', 'c', 'k', '0', '!', 'ë', '‘', '\n', '8', 'j', ']', 's', 'w', '3', '4', '.', 'v', '(', '%', '2', '&', '’', 'n', ',', '7', 'ô', '\ufeff', 'd', '-', 'i', '?', '—', 'u', '•', '5', '*', 'y', 'æ', ' ', ';', '/', '6', '“', 'a', 'e', '#', 'â'}
+    dictionary = dict.fromkeys(character_set, 0)
+    for character in text.lower():
+        dictionary[character] +=1
+    return dictionary
+"""
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text.lower():
+        if c in chars:
+            chars[c] +=1
+        else:
+            chars[c] =1
+    return chars
+
+
+def sort_on(d):
+    return d["count"]
+
+def chars_dict_to_sorted_list(dictionary):
+    sorted_list = []
+    for character in dictionary:
+        sorted_list.append({"character": character, "count": dictionary[character]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
