9699f96e4c226ed255c87673f3f1ef34e4cfb207
bloodsweatncode <christian.schmidt@bluedragon.de>
2025-03-06 17:50:35 +0100

    final version

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_chars_dict, chars_dict_to_sorted_list
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -5,7 +6,10 @@ def get_book_text(filepath):
     
 
 def main():
-    book_path = 'books/frankenstein.txt'
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book = get_book_text(book_path)
     word_count = get_num_words(book)
     chars_dict = get_chars_dict(book)
